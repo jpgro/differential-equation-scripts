cc8cb3a530ca8dc31f884dfb82cbc9c0ab6fc2df
jpgro <juanpabloguerreroe@gmail.com>
2024-07-12 15:27:49 -0600

    Added direction fields plotter script

---
+++ direction_fields.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+n = 1
+x = np.arange(-10, 10, n)
+y = np.arange(-10, 10, n)
+#Rectangular grid with points
+X, Y = np.meshgrid(x, y)
+
+#derivative
+dy = X**2 - Y #Use uppercase
+dx = np.ones(dy.shape)
+
+norm = np.sqrt(dx**2 + dy**2)
+dyu = dy/norm
+dxu = dx/norm
+
+#plot
+plt.quiver(X, Y, dxu, dyu, color = "purple")
+plt.title("Direction field for the differential equation")
+plt.show()
