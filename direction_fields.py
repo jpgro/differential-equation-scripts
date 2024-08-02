fa198808f134865050b045bb5781615d85443cad
jpgro <juanpabloguerreroe@gmail.com>
2024-08-01 23:42:15 -0600

    Reformatted code using black linter

--- direction_fields.py
+++ direction_fields.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 N = 1
 x = np.arange(-10, 10, N)
 y = np.arange(-10, 10, N)
-#Rectangular grid with points
+# Rectangular grid with points
 X, Y = np.meshgrid(x, y)
 
-#derivative
-dy = X**2 - Y #Use uppercase
+# derivative
+dy = X**2 - Y  # Use uppercase
 dx = np.ones(dy.shape)
 
 norm = np.sqrt(dx**2 + dy**2)
-dyu = dy/norm
-dxu = dx/norm
+dyu = dy / norm
+dxu = dx / norm
 
-#plot
-plt.quiver(X, Y, dxu, dyu, color = "purple")
+# plot
+plt.quiver(X, Y, dxu, dyu, color="purple")
 plt.title("Direction field for the differential equation")
 plt.show()
